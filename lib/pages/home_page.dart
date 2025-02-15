00b2a788eb1eec614ae074a12a81312f13969557
Pradeep <pawarpradeep1707@gmail.com>
2025-02-15 21:18:47 +0530

    add three containers with three differnet colors to visulaise it

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:liquid_swipe/liquid_swipe.dart';
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  MyHomePage({super.key});
+
+  final pages = [
+    Container(
+      color: Colors.blue,
+      child: Center(
+        child: Text("Page 1", style: TextStyle(fontSize: 24, color: Colors.white)),
+      ),
+    ),
+    Container(
+      color: Colors.red,
+      child: Center(
+        child: Text("Page 2", style: TextStyle(fontSize: 24, color: Colors.white)),
+      ),
+    ),
+    Container(
+      color: Colors.green,
+      child: Center(
+        child: Text("Page 3", style: TextStyle(fontSize: 24, color: Colors.white)),
+      ),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: LiquidSwipe(
+        pages: pages,
+        enableLoop: true,
+        slideIconWidget: Icon(Icons.arrow_back_ios, color: Colors.white),
+        waveType: WaveType.liquidReveal,
+      ),
+    );
   }
 }
